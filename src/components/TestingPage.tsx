6cd7c72628787aa57eb4a68139be04af295a54bd
dmiles77 <danielmiles89@gmail.com>
2025-03-19 21:35:52 +0200

    fix

--- src/components/TestingPage.tsx
+++ src/components/TestingPage.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Container, Typography } from '@mui/material';
+import { TreeMap, TreeNode } from "miles-tree-map";
+import { worldMap } from '../consts/consts';
 
 const TestingPage: React.FC = () => {
-  return (
-    <Container sx={{ textAlign: 'center', marginTop: 4 }}>
-      <Typography variant="h4">Welcome to the Testing Page</Typography>
-      <Typography variant="body1" sx={{ marginTop: 2 }}>
-        Use the navigation to explore the application.
-      </Typography>
-    </Container>
-  );
+    console.log('World Map:', worldMap); // Log the worldMap object to the console
+    const data: TreeNode = worldMap;
+    return (
+        <Container sx={{ textAlign: 'center', marginTop: 4 }}>
+            <Typography variant="h4">Welcome to the Testing Page</Typography>
+            <Typography variant="body1" sx={{ marginTop: 2 }}>
+                Use the navigation to explore the application.
+            </Typography>
+            <div style={{ width: '100vw', height: '100vh' }}>
+            {/* <div style={{ width: '700px', height: '700px' }}> */}
+                <TreeMap data={data} />
+            </div>
+        </Container>
+    );
 };
 
 export default TestingPage;
